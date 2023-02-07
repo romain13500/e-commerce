52777c1cfb722ae2bcdfa88dcc0b48b0508f2abf
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-07 17:53:04 +0100

    creation template hello

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,6 +6,7 @@ use App\Taxes\Calculator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HelloController{
 
@@ -22,12 +23,13 @@ class HelloController{
     /**
      * @Route("/hello/{prenom?World}", name="hello")
      */
-    public function hello($prenom){
+    public function hello($prenom, Environment $twig){
 
         $this->logger->info("mon message de log !!");
         $tva = $this->calculator->calcul(100);
         dump($tva);
-        return new Response("Salam aleykoum $prenom");
+        $html = $twig->render('hello.html.twig', ['prenom'=>$prenom]);
+        return new Response($html);
 
     }
 }
