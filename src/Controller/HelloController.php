ed49cfa138ebd83aaa5cc9ceafada4321c1425a1
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-07 17:07:22 +0100

    creation container service tva

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController{
+
+    protected $logger;
+    protected $calculator;
+
+
+    public function __construct(LoggerInterface $logger, Calculator $calculator)
+    {
+        $this->logger = $logger;
+        $this->calculator = $calculator;
+    }
 
     /**
      * @Route("/hello/{prenom?World}", name="hello")
      */
     public function hello($prenom){
 
+        $this->logger->info("mon message de log !!");
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         return new Response("Salam aleykoum $prenom");
 
     }
