ed305120ac4db96dd64651858302553bccf6c881
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-08 14:14:38 +0100

    mise en place slugify

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class HelloController{
+
 
     protected $logger;
     protected $calculator;
@@ -22,8 +24,10 @@ class HelloController{
     /**
      * @Route("/hello/{prenom?World}", name="hello")
      */
-    public function hello($prenom, LoggerInterface $logger, Calculator $calculator, Environment $twig){
+    public function hello($prenom, LoggerInterface $logger, Calculator $calculator, Slugify $slugify, Environment $twig){
 
+       
+        dump($slugify->slugify("hey"));
         $logger->info("mon message de log !!");
         $tva = $calculator->calcul(100);
         dump($tva);
