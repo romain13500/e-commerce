0cac866c398ea58441b0a191f236842d8eab918d
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-10 11:57:38 +0100

    Update HelloController.php

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
+
 
 class HelloController extends AbstractController {
 
