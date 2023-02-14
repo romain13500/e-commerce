79c2501d4cdf0646359d6739266cc96cc7ef64fa
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-14 11:36:18 +0100

    mise en place faker dans bdd

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,16 +5,19 @@ namespace App\DataFixtures;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('fr_FR');
+
         for ($p=0; $p < 100 ; $p++) { 
             $product = new Product;
-            $product->setName("Produit n°$p")
+            $product->setName($faker->sentence())
                 ->setPrice(mt_rand(100, 200))
-                ->setSlug("produit-n-$p");
+                ->setSlug($faker->slug());
 
             $manager->persist($product);
         }
