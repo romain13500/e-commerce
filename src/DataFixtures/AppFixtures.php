c2ac8cc026a6b8c60459709228850bd981b8b47f
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-14 11:45:18 +0100

    ajout librairie faker price et faker collection
    composer require mbezhanov/laravel-faker-provider-collection
    
    composer require liorchamla/faker-prices

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,11 +12,13 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+        $faker->addProvider( new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
 
         for ($p=0; $p < 100 ; $p++) { 
             $product = new Product;
-            $product->setName($faker->sentence())
-                ->setPrice(mt_rand(100, 200))
+            $product->setName($faker->productName)
+                ->setPrice($faker->price(4000, 2000))
                 ->setSlug($faker->slug());
 
             $manager->persist($product);
