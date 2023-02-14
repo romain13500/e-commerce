454799b767803d6fd325571e01719ac427eaaffe
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-14 12:45:23 +0100

    ajout picture dans bdd et faker picsum
    composer require bluemmb/faker-picsum-photos-provider

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -23,6 +23,7 @@ class AppFixtures extends Fixture
         $faker = Factory::create('fr_FR');
         $faker->addProvider( new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
         for ($c=0; $c < 3; $c++) { 
             $category = new Category;
@@ -37,7 +38,10 @@ class AppFixtures extends Fixture
                 $product->setName($faker->productName)
                     ->setPrice($faker->price(4000, 2000))
                     ->setSlug(strtolower($this->slugger->slug($product->getName())))
-                    ->setCategory($category);
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->imageUrl(400, 400, true));
+                    
     
                 $manager->persist($product);
             } 
