0ce20e370064c32a46acfac01245935586af6592
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-27 17:29:53 +0100

    login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="security_login")
      */
-    public function index()
+    public function login(AuthenticationUtils $utils)
     {
+        $form = $this->createForm(LoginType::class, ['email' => $utils->getLastUsername()]);
         return $this->render('security/login.html.twig', [
-            
+           'formView' => $form->createView(),
+           'error' => $utils->getLastAuthenticationError() 
         ]);
     }
 }
