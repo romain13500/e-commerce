2eb835f67b92c44f955d22c19c38953d947ef35e
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-27 10:51:15 +0100

    Update CategoryController.php

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,7 +8,6 @@ use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
