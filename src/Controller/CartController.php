a4ce0b8d788e47d7b6fed3ee118646738ba6de8f
Mzungu 13 <107195039+romain13500@users.noreply.github.com>
2023-02-28 12:06:08 +0100

    cart session

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,18 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="app_cart")
+     * @Route("/cart/add/{id}", name="cart_add")
      */
-    public function index(): Response
+    public function add($id, Request $request)
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        //  1 - Retrouver le panier dans la session (sous forme de tableau)
+        //  2 - Si inexistant, prendre un tableau vide
+        $cart = $request->getSession()->get('cart', []);
+
+        //  3 - Voir si le product (id) existe déjà dans le tableau
+        //  4 - Si il existe, augmenter la quantité
+        //  5 - Sinon ajouter le product en quantité de 1
+        if (array_key_exists($id, $cart)) {
+            $cart[$id]++;
+        }else {
+            $cart[$id] = 1;
+        }
+
+        //  6 - Enregistrer le tableau
+        $request->getSession()->set('cart', $cart);
+
+        dd($request->getSession()->get('cart'));
+
     }
 }
